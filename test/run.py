a80ecb6ac46f66b57c215d0c9871b3e3d2d4ee52
dyq666 <694526892@qq.com>
2018-05-10 21:30:46 +0800

    为测试代码增加注释

--- test/run.py
+++ test/run.py
@@ -6,9 +6,14 @@ from interpreter.outputRes import OutputRes
 
 file_choice = 2
 file_path = 'sub_test' if file_choice == 1 else 'test_code.py'
-# 读入要执行的代码
+# 从文件读入要执行的代码, 也可以直接写字符串
 with open(file_path, 'r') as f:
     data = f.read()
 
 output_res = OutputRes(data)
 res = output_res.get_Res()
+
+# 输出相应的值
+# print(res['lex'])
+# print(res['yacc'])
+# print(res['exe'])
