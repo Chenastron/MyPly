92e8b79edc5f8e731b485955f2c5d953e266c943
dyq666 <694526892@qq.com>
2018-05-10 21:19:39 +0800

    项目结构基本优化完成(除了yacc部分的rule还未全部拆分)向外界仅提供一个类

---
+++ interpreter/outputRes.py
@@ -0,0 +1,47 @@
+# _*_ coding: utf-8 _*_
+__author__ = "dyq666"
+__date__ = "2018/5/10 19:25"
+
+
+from ply import lex, yacc
+
+from interpreter.myLex import dlexer
+from interpreter.myYacc import dyacc
+from interpreter.myExe.dexecute import DyqExecute
+
+
+class OutputRes:
+    """
+    1. 外界使用这个module仅需要声明这个类的实例
+    2. 创建实例时传入一个待解析的字符串
+    3. 使用实例方法get_Res()得到结果, 结果为一个dict三个key分别为‘lex’ ‘yacc' 'exe'
+    """
+    def __init__(self, data):
+        # 读入词法分析器和语法分析器
+        self.lexor = lex.lex(module=dlexer)
+        self.yaccor = yacc.yacc(module=dyacc)
+        # 读入要被解析的字符串
+        self.data = data
+
+    def _evaluate_lex(self):
+        """计算词法分析器的结果"""
+        self.lexor.input(self.data)
+        return '\n'.join([str(single_lex) for single_lex in self.lexor])
+
+    def _evaluate_yacc(self):
+        """计算语法分析器的结果"""
+        return '\n'.join([str(single_yacc) for single_yacc in self.yaccor.parse(self.data)])
+
+    def _evaluate_exe(self):
+        """计算最终执行的结果"""
+        for x in self.yaccor.parse(self.data):
+            DyqExecute.resolve(x)
+        return '\n'.join(DyqExecute.res_string)
+
+    def get_Res(self):
+        """返回结果"""
+        return {
+            'lex': self._evaluate_lex(),
+            'yacc': self._evaluate_yacc(),
+            'exe': self._evaluate_exe()
+        }
--- test/run.py
+++ test/run.py
@@ -1,37 +1,8 @@
 # _*_ coding: utf-8 _*_
-__author__ = 'daiyaquan'
-__date__ = '2018/4/5 19:37'
+__author__ = "dyq666"
+__date__ = "2018/5/10 21:07"
 
-
-from ply import lex, yacc
-
-from interpreter.myLex import dlexer
-from interpreter.myYacc import dparser
-from interpreter.myExe.dexecute import DyqExecute
-
-# 读入词法分析器和语法分析器
-lexs = lex.lex(module=dlexer)
-parses = yacc.yacc(module=dparser)
-
-
-def show_lex():
-    """显示词法分析器的结果"""
-    lexs.input(data)
-    for l in lexs:
-        print(l)
-
-
-def show_parse():
-    """显示语法分析器的结果"""
-    for x in parses.parse(data):
-        print(x)
-
-
-def show_exe():
-    """显示最终执行的结果"""
-    for x in parses.parse(data):
-        DyqExecute.resolve(x)
-    print('\n'.join(DyqExecute.res_string))
+from interpreter.outputRes import OutputRes
 
 file_choice = 2
 file_path = 'sub_test' if file_choice == 1 else 'test_code.py'
@@ -39,12 +10,5 @@ file_path = 'sub_test' if file_choice == 1 else 'test_code.py'
 with open(file_path, 'r') as f:
     data = f.read()
 
-
-show_dict = {
-    1: show_lex,
-    2: show_parse,
-    3: show_exe
-}
-
-choice = 3
-show_dict[choice]()
+output_res = OutputRes(data)
+res = output_res.get_Res()
