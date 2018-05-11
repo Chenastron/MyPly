61133ba1510e7601ed6232b05461d4d7a0e35781
dyq666 <694526892@qq.com>
2018-05-11 17:04:04 +0800

    前端界面基础版本

---
+++ frontend/src/pages/index/components/content.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="content-wrapper">
+    <div class="input-title">
+      <el-select v-model="selectedValue" placeholder="请选择">
+        <el-option
+          v-for="item in inputOptions"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <el-button type="primary" plain @click="commitInput">
+        运行
+      </el-button>
+    </div>
+    <div class="input-wrapper">
+      <el-input
+        class="input"
+        type="textarea"
+        :rows="20"
+        placeholder="请输入内容"
+        v-model="inputArea">
+      </el-input>
+      <el-input
+        class="output"
+        type="textarea"
+        :rows="20"
+        :disabled="true"
+        v-model="outputArea">
+      </el-input>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  data () {
+    return {
+      inputOptions: [
+        {
+          label: '词法分析',
+          value: 'lex'
+        },
+        {
+          label: '语法分析',
+          value: 'parse'
+        },
+        {
+          label: '执行结果',
+          value: 'exe'
+        }
+      ],
+      selectedValue: 'exe',
+      inputArea: '',
+      outputArea: ''
+    }
+  },
+  methods: {
+    commitInput () {
+      console.log(this.inputArea)
+      console.log(this.selectedValue)
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+#content-wrapper
+  padding 0 10vw
+  display flex
+  justify-content space-between
+  flex-direction column
+  .input-title
+    width 48%
+    display flex
+    justify-content space-between
+    margin-bottom 2vh
+  .input-wrapper
+    display flex
+    justify-content space-between
+    width 100%
+    .input
+      width 48%
+    .output
+      width 48%
+</style>
---
+++ frontend/src/pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="index-wrapper">
+    <div id="cover"></div>
+    <Header/>
+    <Content/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue'
+import Footer from './components/footer.vue'
+import Content from './components/content.vue'
+
+export default {
+  components: {
+    Header,
+    Footer,
+    Content
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  #index-wrapper
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+  #cover
+    position absolute
+    left 0
+    top 0
+    right 0
+    bottom 0
+    background-color #999
+    opacity .9
+    z-index -1
+</style>
