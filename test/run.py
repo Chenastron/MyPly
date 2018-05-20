80246b0297dfb2033acd05a0d197aff59e441811
dyq666 <694526892@qq.com>
2018-05-20 15:07:44 +0800

    优化笔记

--- test/run.py
+++ test/run.py
@@ -4,7 +4,7 @@ __date__ = "2018/5/10 21:07"
 
 from interpreter.outputRes import OutputRes
 
-file_choice = 1
+file_choice = 2
 file_path = 'sub_test' if file_choice == 1 else 'test_code.py'
 
 # # 从文件读入要执行的代码, 也可以直接写字符串
@@ -16,14 +16,12 @@ with open(file_path, 'r') as f:
 
 # OutputRes(data)
 output_res = OutputRes(data)
-# res = output_res.get_res()
+res = output_res.get_res()
 # res = output_res._evaluate_lex()
 # res = output_res._evaluate_yacc()
-res = output_res._evaluate_exe()
+# res = output_res._evaluate_exe()
 
 # 输出相应的值
-# print(res['lex'])
-# print(res['yacc'])
-# print(res['exe'])
-
-print(res)
+print(res['lex'])
+print(res['yacc'])
+print(res['exe'])
